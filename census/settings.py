dceba642bdcee2cf14d1ac9824d26313ccab5644
Kristian John Baturiano <kjqb4929@gmail.com>
2020-10-15 20:11:15 +0800

    Second commit

--- census/settings.py
+++ census/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'census.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
 
