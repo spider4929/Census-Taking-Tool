ed516e165d0a8ea3919087f6631c9d35077828b9
Kristian John Baturiano <kjqb4929@gmail.com>
2020-10-25 17:35:57 +0800

    Second commit

--- forms/models.py
+++ forms/models.py
@@ -1,48 +1,49 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
+from django.conf import settings
 
-GENDER = [
-    ('MALE', 'MALE'),
-    ('FEMALE', 'FEMALE'),
-]
-CIVIL_STATUS = [
-    ('MARRIED', 'MARRIED'),
-    ('SINGLE', 'SINGLE'),
-    ('SEPARATED', 'SEPARATED'),
-    ('WIDOWED', 'WIDOWED'),
-]
-CITIZENSHIP = [
-    ('FILIPINO', 'FILIPINO'),
-    ('FOREIGNER', 'FOREIGNER'),
-]
-NAME_EXTENSION = [
+GENDER = (
+    ('MALE', 'Male'),
+    ('FEMALE', 'Male'),
+)
+CIVIL_STATUS = (
+    ('MARRIED', 'Married'),
+    ('SINGLE', 'Single'),
+    ('SEPARATED', 'Separated'),
+    ('WIDOWED', 'Widowed'),
+)
+CITIZENSHIP = (
+    ('FILIPINO', 'Filipino'),
+    ('FOREIGNER', 'Foreigner'),
+)
+NAME_EXTENSION = (
     (' JR.', 'Jr.'),
     (' SR.', 'Sr.'),
     (' II', 'II'),
     (' III', 'III'),
-]
+)
 
 characters = RegexValidator(r'^[a-zA-Z Ññ]*$', message='Only letters are allowed.')
 fullname_characters = RegexValidator(r'^[a-zA-Z Ññ]*$', message='Only letters are allowed.')
 digits = RegexValidator(r'^[0-99999]*$', message='Only numbers are allowed.')
 
 
-class Person(models.Model):  # TODO: add user accountability
-    household_no = models.CharField(max_length=10, validators=[digits], help_text='Enter household number here.')
-    full_name = models.CharField(max_length=50, unique=True, validators=[fullname_characters])  # full name, formatted like so {{Surname} {Extension}, {First Name} {Middle Name}}
-    last_name = models.CharField(max_length=20, help_text='Enter surname here.', validators=[characters])  # surname
-    first_name = models.CharField(max_length=20, help_text='Enter first name here.', validators=[characters])  # first name
-    middle_name = models.CharField(max_length=20, blank=True, help_text='Enter middle name here.', validators=[characters])  # middle name
+class Person(models.Model):
+    household_no = models.SmallIntegerField(help_text='Enter household number here.')
+    full_name = models.CharField(max_length=240, unique=True, validators=[fullname_characters])  # full name, formatted like so {{Surname} {Extension}, {First Name} {Middle Name}}
+    last_name = models.CharField(max_length=128, help_text='Enter surname here.', validators=[characters])  # surname
+    first_name = models.CharField(max_length=128, help_text='Enter first name here.', validators=[characters])  # first name
+    middle_name = models.CharField(max_length=64, blank=True, help_text='Enter middle name here.', validators=[characters])  # middle name
     name_extension = models.CharField(max_length=4, blank=True, choices=NAME_EXTENSION, help_text='Choose name extension, if required.')  # name extensions (Sr., Jr., III, IV, etc.)
     address = models.CharField(max_length=100, help_text='Enter house no. and street no.')  # house no. and street name only
     birth_place = models.CharField(max_length=20, help_text='Enter city of birth only.')  # birth place, typically a city
     birth_date = models.DateField()  # birth date
     gender = models.CharField(max_length=6, choices=GENDER, help_text='Choose gender.')  # gender, male or female
-    civil_status = models.CharField(max_length=10, choices=CIVIL_STATUS, help_text='Choose civil status.')  # married, single, divorced, widowed
-    citizenship = models.CharField(max_length=10, choices=CITIZENSHIP, help_text='Choose citizenship.')  # Filipino or Foreigner
+    civil_status = models.CharField(max_length=9, choices=CIVIL_STATUS, help_text='Choose civil status.')  # married, single, divorced, widowed
+    citizenship = models.CharField(max_length=9, choices=CITIZENSHIP, help_text='Choose citizenship.')  # Filipino or Foreigner
     occupation = models.CharField(max_length=40, help_text='Enter occupation here.')  # job
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)  # TODO: do something about the NULL
+    author = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.full_name
